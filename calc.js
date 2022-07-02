468e48713a466442297dbbde6054afb8a1a7e049
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2022-07-02 03:53:23 -0500

    pretty much finished the layout, mostly need to plug in the buttons to JS and operation

--- calc.js
+++ calc.js
@@ -1,3 +1,10 @@
+let mode = "picker";
+//let mouseDown = false;
+//let selectedButton = document.querySelector('#picker')
+
+const readout = document.querySelector('.readout');
+const input = document.querySelector('.input');
+
 function add(a, b) {
     return a + b;
 }
@@ -31,6 +38,43 @@ function operate(operator, a, b) {
     return result;
 }
 
+function popDigitButtons() {
+    let count1 = 0;
+    const digits = document.createElement('div');
+    digits.classList.add('digits');
+    input.append(digits);
+    for(let i = 0; i <= 9; i++) {
+        const numbers = document.createElement('button');
+        numbers.innerHTML = `${count1}`;
+        numbers.classList.add('numberB');
+        digits.append(numbers);
+        count1++;
+    }
+    digits.style.gridTemplateColumns = 'repeat(100px, 1fr)';
+    digits.style.gridTemplateRows = 'repeat(100px, 1fr)';
+}
+
+function popOperatorButtons() {
+    const operator = document.createElement('div');
+    operator.classList.add('operators');
+    input.append(operator);
+    let operations = ['+', '-', '*', '/', '='];
+    for (let i = 0; i < operations.length; i++) {
+        const ops = document.createElement('button');
+        ops.innerHTML = `${operations[i]}`;
+        ops.classList.add(`operatorB${i}`);
+        operator.append(ops);
+    }
+}
+
+function popDisplay() {
+    let displayVal;
+    
+}
+
+function changeMode(btn) {
+    
+}
 //console.logs for testing - should all equal 12
 console.log(add(9, 3));
 console.log(subtract(16, 4));
@@ -40,3 +84,7 @@ console.log(operate(add, 9, 3));
 console.log(operate(divide, 48, 4));
 console.log(operate(subtract, 15, 3));
 console.log(operate(multiply, 3, 4));
+
+// start
+popDigitButtons();
+popOperatorButtons();
